643517cf777bdeadba3552037763a341b5ff60c5
Christopher Stevers <christopher.stevers@protonmail.com>
2026-03-08 05:59:44 -0400

    load env differently

--- web/next.config.js
+++ web/next.config.js
@@ -1,13 +1,18 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
+import { loadEnvConfig } from "@next/env";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 import "./src/env.js";
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const projectRoot = path.resolve(__dirname, "..");
+
+// Load .env from repo root (one level above /web)
+loadEnvConfig(projectRoot);
 
 /** @type {import("next").NextConfig} */
 const config = {
